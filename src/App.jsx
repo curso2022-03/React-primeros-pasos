30b5b2892a67e9bcfa2e3628c8678c14e98213c5
dam219 <peib29@gmail.com>
2022-01-11 10:26:57 +0100

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import { PaginaListas } from './components/paginaListas';
 import { Home } from './components/Home';
 import { Perfil } from './components/Perfil';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export function App() {
   return (
