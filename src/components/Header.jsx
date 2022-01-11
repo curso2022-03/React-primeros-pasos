b524816aca5ad882ee8cf298e865fbe23a1b011b
dam219 <peib29@gmail.com>
2022-01-11 12:22:40 +0100

    2 files modified, 1 added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,66 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { MenuItems } from '../data/MenuItems';
 
-export default function Header(props) {
-  return (
-    <header>
-      <div className="logo-text">
-        <img
-          src="https://hdwallpaperim.com/wp-content/uploads/2017/08/25/461264-reactJS-Facebook-JavaScript-minimalism-artwork-simple_background-748x421.jpg"
-          width="200"
-          height="150"
-          id="logo"
-          alt="logo"
-        />
-        <h1 className="text-header">Usando react</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/listas">Lista de tareas</Link>
-          </li>
-          <li>
-            <Link to="/perfil">Perfil</Link>
-          </li>
-        </ul>
-      </div>
-    </header>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  /**
+   * Forma menu nº1: bucle for
+   */
+  createMenuLinks() {
+    const listaComponentes = [];
+    for (let i = 0; i < MenuItems.length; i++) {
+      listaComponentes.push(
+        <li>
+          <Link to={MenuItems[i].path}>{MenuItems[i].title}</Link>
+        </li>
+      );
+    }
+    return listaComponentes;
+  }
+  render() {
+    return (
+      <header>
+        <div className="logo-text">
+          <img
+            src="https://hdwallpaperim.com/wp-content/uploads/2017/08/25/461264-reactJS-Facebook-JavaScript-minimalism-artwork-simple_background-748x421.jpg"
+            width="200"
+            height="150"
+            id="logo"
+            alt="logo"
+          />
+          <h1 className="text-header">Usando react</h1>
+          {/**
+           * Para la forma 1:
+           *  <ul>{this.createMenuLinks()}</ul>
+           */}
+          {/**
+           * Forma 2:map (funcion)
+           * {MenuItems.map(function(item){
+           * return (
+           * <li>
+           *  <Link to={item.path}>{item.title}</Link>
+           * </li>
+           * );
+           * })}
+           * </ul>
+           */}
+          {/**
+           * Forma 3:map (funcion flecha)
+           */}
+          {MenuItems.map((item) => {
+            return (
+              <li>
+                <Link to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </header>
+    );
+  }
 }
+export default Header;
---
+++ src/data/MenuItems.js
@@ -0,0 +1,14 @@
+import { PaginaListas } from '../components/paginaListas';
+import { Home } from '../components/Home';
+import { Perfil } from '../components/Perfil';
+
+export const MenuItems = [
+  { id: 1, path: '/', title: 'Home', component: Home },
+  {
+    id: 2,
+    path: '/listas',
+    title: 'Listas de tareas',
+    component: PaginaListas,
+  },
+  { id: 3, path: '/perfil', title: 'Perfil', component: Perfil },
+];
