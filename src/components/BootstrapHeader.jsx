ad5739a52b97c522129ce6cb158e020e479ab44a
dam219 <peib29@gmail.com>
2022-01-17 13:55:50 +0100

    Añadido tabla con Bootstrap

--- src/components/BootstrapHeader.jsx
+++ src/components/BootstrapHeader.jsx
@@ -12,7 +12,7 @@ class BootstrapHeader extends React.Component {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to={MenuItems[0].path}>
             <img
               className="logo-image"
               src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BootstrapTabla from './BootstrapTabla';
 
 export function Perfil() {
   return (
     <div className="main-site">
       <h1> Pagina de Perfil</h1>
+      <BootstrapTabla />
     </div>
   );
 }
